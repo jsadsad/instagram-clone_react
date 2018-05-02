8f0b0427952f8a97e39f7bcdf52a9ed8380332c8
Joshua Clarence D Sadsad <joshsad2@gmail.com>
2018-05-02 01:58:50 -0700

    full add

---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Header extends React.Component{
+  render() {
+    return (
+      <nav className="nav">
+        <div className="Nav-menus">
+          <div className="Nav-brand">
+            <a className="Nav-brand-logo" href="/">Instagram</a>
+          </div>
+        </div>
+      </nav>
+    )
+  }
+}
+
+export default Header;
